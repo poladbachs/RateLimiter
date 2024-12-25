5293ac58ac479d84db09e0e63b363250e70f0d37
Polad <bakhip@usi.ch>
2024-12-25 03:17:43 +0100

    init file with wrap obj and scale limits

---
+++ rate_limiter/__init__.py
@@ -0,0 +1,32 @@
+import copy
+import re
+from .rate_limiter_group import RateLimiterGroup
+
+def wrap_object(object=None, rate_limiter_group=None, wrap_defs=None):
+    wrap_defs = copy.deepcopy(wrap_defs)
+
+    methods = dir(object)
+    for method in methods:
+        matched_defs = _extract_matched_defs(method=method, wrap_defs=wrap_defs)
+
+        if len(matched_defs) == 0:
+            continue
+
+        def get_wrapped_method(orig_method=None, matched_defs=None):
+            def wrapped_method(*args, **kwargs):
+                for matched_def in matched_defs:
+                    rate_limiter_group.rate_limit(tags=matched_def['tags'], count=matched_def['count'])
+                return orig_method(*args, **kwargs)
+            return wrapped_method
+
+        orig_method = getattr(object, method)
+        setattr(object, method, get_wrapped_method(orig_method=orig_method, matched_defs=matched_defs))
+
+def scale_limits(limits=None, scale=None):
+    limits = copy.deepcopy(limits)
+    for limit in limits:
+        limit['count'] *= scale
+        if limit['count'] < 1:
+            limit['period_sec'] /= limit['count']
+            limit['count'] = 1
+    return limits
